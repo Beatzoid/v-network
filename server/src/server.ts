c7601c6f8d6c233ca432e39aaec47cfef5e2fd34
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2021-07-02 13:07:16 -0700

    🔒️ Fix Security Issue

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import csurf from "csurf";
 
 import logger from "./utils/logger";
 import authRoutes from "./routes/authRoute";
@@ -11,6 +12,7 @@ import authRoutes from "./routes/authRoute";
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(csurf({ cookie: true }));
 app.use(cors());
 
 // Routes
