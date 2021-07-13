2d2d30ec0f9765c0e80e51bdc6166e1ad386c283
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2021-07-13 08:13:56 -0700

    👷 Fix CI Build

--- client/src/components/header/Search.tsx
+++ client/src/components/header/Search.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
 
 import { IUser } from "../../redux/types/auth";
 import { GLOBALTYPES, useAppSelector } from "../../redux/types/global";
